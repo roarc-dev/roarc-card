7fa184527e40c795acafadb0a924a9a51d92725f
roarc-dev <roarc.official@gmail.com>
2026-01-10 23:22:21 +0900

    fix: 계좌 정보 필드명 수정 (groom_name → groom_bank_name)

--- components/wedding/Account.tsx
+++ components/wedding/Account.tsx
@@ -74,26 +74,32 @@ interface AccountInfo {
     groom_phone: string
     groom_account: string
     groom_bank: string
+    groom_bank_name: string
     groom_father_name: string
     groom_father_phone: string
     groom_father_account: string
     groom_father_bank: string
+    groom_father_bank_name: string
     groom_mother_name: string
     groom_mother_phone: string
     groom_mother_account: string
     groom_mother_bank: string
+    groom_mother_bank_name: string
     bride_name: string
     bride_phone: string
     bride_account: string
     bride_bank: string
+    bride_bank_name: string
     bride_father_name: string
     bride_father_phone: string
     bride_father_account: string
     bride_father_bank: string
+    bride_father_bank_name: string
     bride_mother_name: string
     bride_mother_phone: string
     bride_mother_account: string
     bride_mother_bank: string
+    bride_mother_bank_name: string
     created_at?: string
     updated_at?: string
 }
@@ -147,26 +153,32 @@ export default function AccountBtn(props: AccountBtnProps) {
                 groom_phone: '010-1234-5678',
                 groom_account: '123-456-789012',
                 groom_bank: '국민은행',
+                groom_bank_name: '김철수',
                 groom_father_name: '김아빠',
                 groom_father_phone: '010-9876-5432',
                 groom_father_account: '987-654-321098',
                 groom_father_bank: '신한은행',
+                groom_father_bank_name: '김아빠',
                 groom_mother_name: '김엄마',
                 groom_mother_phone: '010-1111-2222',
                 groom_mother_account: '111-222-333444',
                 groom_mother_bank: '하나은행',
+                groom_mother_bank_name: '김엄마',
                 bride_name: '이영희',
                 bride_phone: '010-5555-6666',
                 bride_account: '456-789-123456',
                 bride_bank: '우리은행',
+                bride_bank_name: '이영희',
                 bride_father_name: '이아빠',
                 bride_father_phone: '010-7777-8888',
                 bride_father_account: '777-888-999000',
                 bride_father_bank: '농협',
+                bride_father_bank_name: '이아빠',
                 bride_mother_name: '이엄마',
                 bride_mother_phone: '010-3333-4444',
                 bride_mother_account: '321-654-987654',
                 bride_mother_bank: '하나은행',
+                bride_mother_bank_name: '이엄마',
             }
             setAccountInfo(dummyAccountInfo)
             setLoading(false)
@@ -250,42 +262,42 @@ export default function AccountBtn(props: AccountBtnProps) {
     // 계좌 정보 복사 함수들
     const copyGroomAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.groom_bank} ${accountInfo.groom_account} ${accountInfo.groom_name}`
+            const text = `${accountInfo.groom_bank} ${accountInfo.groom_account} ${accountInfo.groom_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
 
     const copyGroomFatherAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.groom_father_bank} ${accountInfo.groom_father_account} ${accountInfo.groom_father_name}`
+            const text = `${accountInfo.groom_father_bank} ${accountInfo.groom_father_account} ${accountInfo.groom_father_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
 
     const copyGroomMotherAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.groom_mother_bank} ${accountInfo.groom_mother_account} ${accountInfo.groom_mother_name}`
+            const text = `${accountInfo.groom_mother_bank} ${accountInfo.groom_mother_account} ${accountInfo.groom_mother_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
 
     const copyBrideAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.bride_bank} ${accountInfo.bride_account} ${accountInfo.bride_name}`
+            const text = `${accountInfo.bride_bank} ${accountInfo.bride_account} ${accountInfo.bride_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
 
     const copyBrideFatherAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.bride_father_bank} ${accountInfo.bride_father_account} ${accountInfo.bride_father_name}`
+            const text = `${accountInfo.bride_father_bank} ${accountInfo.bride_father_account} ${accountInfo.bride_father_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
 
     const copyBrideMotherAccount = () => {
         if (accountInfo) {
-            const text = `${accountInfo.bride_mother_bank} ${accountInfo.bride_mother_account} ${accountInfo.bride_mother_name}`
+            const text = `${accountInfo.bride_mother_bank} ${accountInfo.bride_mother_account} ${accountInfo.bride_mother_bank_name}`
             copyToClipboard(text, "복사되었습니다")
         }
     }
@@ -635,21 +647,21 @@ const GroomAccountButton = React.memo(function GroomAccountButton({
                             const groomAccounts = [
                                 {
                                     condition:
-                                        accountInfo.groom_name &&
+                                        accountInfo.groom_bank_name &&
                                         accountInfo.groom_account &&
                                         accountInfo.groom_bank,
                                     component: "groom",
                                 },
                                 {
                                     condition:
-                                        accountInfo.groom_father_name &&
+                                        accountInfo.groom_father_bank_name &&
                                         accountInfo.groom_father_account &&
                                         accountInfo.groom_father_bank,
                                     component: "groom_father",
                                 },
                                 {
                                     condition:
-                                        accountInfo.groom_mother_name &&
+                                        accountInfo.groom_mother_bank_name &&
                                         accountInfo.groom_mother_account &&
                                         accountInfo.groom_mother_bank,
                                     component: "groom_mother",
@@ -669,7 +681,7 @@ const GroomAccountButton = React.memo(function GroomAccountButton({
                                         <AccountItem
                                             key="groom"
                                             label="신랑"
-                                            name={accountInfo.groom_name}
+                                            name={accountInfo.groom_bank_name}
                                             account={accountInfo.groom_account}
                                             bank={accountInfo.groom_bank}
                                             onCopy={onCopyGroom}
@@ -682,7 +694,7 @@ const GroomAccountButton = React.memo(function GroomAccountButton({
                                         <AccountItem
                                             key="groom_father"
                                             label="혼주"
-                                            name={accountInfo.groom_father_name}
+                                            name={accountInfo.groom_father_bank_name}
                                             account={
                                                 accountInfo.groom_father_account
                                             }
@@ -697,7 +709,7 @@ const GroomAccountButton = React.memo(function GroomAccountButton({
                                         <AccountItem
                                             key="groom_mother"
                                             label="혼주"
-                                            name={accountInfo.groom_mother_name}
+                                            name={accountInfo.groom_mother_bank_name}
                                             account={
                                                 accountInfo.groom_mother_account
                                             }
@@ -840,21 +852,21 @@ const BrideAccountButton = React.memo(function BrideAccountButton({
                             const brideAccounts = [
                                 {
                                     condition:
-                                        accountInfo.bride_name &&
+                                        accountInfo.bride_bank_name &&
                                         accountInfo.bride_account &&
                                         accountInfo.bride_bank,
                                     component: "bride",
                                 },
                                 {
                                     condition:
-                                        accountInfo.bride_father_name &&
+                                        accountInfo.bride_father_bank_name &&
                                         accountInfo.bride_father_account &&
                                         accountInfo.bride_father_bank,
                                     component: "bride_father",
                                 },
                                 {
                                     condition:
-                                        accountInfo.bride_mother_name &&
+                                        accountInfo.bride_mother_bank_name &&
                                         accountInfo.bride_mother_account &&
                                         accountInfo.bride_mother_bank,
                                     component: "bride_mother",
@@ -874,7 +886,7 @@ const BrideAccountButton = React.memo(function BrideAccountButton({
                                         <AccountItem
                                             key="bride"
                                             label="신부"
-                                            name={accountInfo.bride_name}
+                                            name={accountInfo.bride_bank_name}
                                             account={accountInfo.bride_account}
                                             bank={accountInfo.bride_bank}
                                             onCopy={onCopyBride}
@@ -887,7 +899,7 @@ const BrideAccountButton = React.memo(function BrideAccountButton({
                                         <AccountItem
                                             key="bride_father"
                                             label="혼주"
-                                            name={accountInfo.bride_father_name}
+                                            name={accountInfo.bride_father_bank_name}
                                             account={
                                                 accountInfo.bride_father_account
                                             }
@@ -902,7 +914,7 @@ const BrideAccountButton = React.memo(function BrideAccountButton({
                                         <AccountItem
                                             key="bride_mother"
                                             label="혼주"
-                                            name={accountInfo.bride_mother_name}
+                                            name={accountInfo.bride_mother_bank_name}
                                             account={
                                                 accountInfo.bride_mother_account
                                             }
